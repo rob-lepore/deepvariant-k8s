36a4d4e445d8937a36bb50b6bbc16992d1b7c47b
Roberto Lepore <roberto.lepore.rl@gmail.com>
2025-11-07 15:00:05 +0100

    simple k8s pipeline

---
+++ run.sh
@@ -0,0 +1,11 @@
+# Upload files to minikube
+minikube cp pipeline/nextflow.config /data/shared/nextflow.config
+minikube cp pipeline/main.nf /data/shared/main.nf
+minikube cp pipeline/data/a.txt /data/shared/data/a.txt
+minikube cp pipeline/data/b.txt /data/shared/data/b.txt
+minikube cp pipeline/data/c.txt /data/shared/data/c.txt
+
+
+# Run Nextflow
+kubectl delete pod nextflow-hello
+kubectl apply -f pod.yaml
